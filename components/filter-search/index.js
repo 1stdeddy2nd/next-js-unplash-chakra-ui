6a6a9eccf4a6814426481c0559672bbb3b4ae8ba
1stdeddy2nd <deddysetiawan17@gmail.com>
2022-01-05 16:59:25 +0700

    [FETCH] able to fetch per page from unplash API depend on url param

--- components/filter-search/index.js
+++ components/filter-search/index.js
@@ -6,36 +6,35 @@ import { VscSettings, VscSearch } from 'react-icons/vsc';
 export default function FilterSearch({ placeholder, onFilter, onSearch }) {
   return (
     <InputGroup size="md">
-      <InputLeftElement width="4.5rem" height="29px">
+      <InputLeftElement width="4.5rem" marginLeft="3px">
         <Button
           leftIcon={<VscSettings style={{ transform: 'rotate(90deg)' }} />}
-          size="xs"
+          size="sm"
           backgroundColor="white"
           shadow="sm"
-          borderRadius="2px"
+          borderRadius="3px"
           _hover={{ bg: 'gray.500', color: 'gray.100' }}
           onClick={onFilter}
         >
-          <Text marginTop="2px" fontSize="11px">Filter</Text>
+          <Text marginTop="2px" fontSize="13px">Filter</Text>
         </Button>
       </InputLeftElement>
       <Input
         type="text"
         placeholder={placeholder}
         paddingLeft="5rem"
-        fontSize="11px"
-        borderRadius="2px"
+        fontSize="13px"
+        borderRadius="3px"
         backgroundColor="gray.100"
-        focusBorderColor="gray.500"
-        height="30px"
+        focusBorderColor="gray.300"
       />
-      <InputRightElement height="29px" marginRight="-5px">
+      <InputRightElement>
         <IconButton
-          icon={<VscSearch size="11px" />}
+          icon={<VscSearch />}
           backgroundColor="white"
-          size="xs"
+          size="sm"
           shadow="sm"
-          borderRadius="2px"
+          borderRadius="3px"
           _hover={{ bg: 'gray.500', color: 'gray.100' }}
           onClick={onSearch}
         />
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,49 @@
 import {
   Container,
   Box,
+  Image,
+  Grid,
+  GridItem,
 } from '@chakra-ui/react';
 import FilterSearch from '../components/filter-search';
 
-export default function CallToActionWithAnnotation() {
+export default function Photo(props) {
+  const { dataPhotos } = props;
   return (
-    <Container maxW="container.lg" alignItems="center" display="flex" justifyContent="center">
-      <Box marginY="10px" w="100%">
-        <FilterSearch placeholder="Search knowledge" />
+    <Container maxW="container.lg" alignItems="center" display="flex" justifyContent="center" paddingX="20px">
+      <Box marginY="20px" w="100%">
+        <FilterSearch placeholder="Search image" />
+        <Grid
+          marginTop="30px"
+          templateColumns="repeat(3, 1fr)"
+          gap={2}
+          w="100%"
+        >
+          {dataPhotos.map((item) => (
+            <GridItem colSpan={1} rowSpan={item.height > item.width ? 2 : 0} key={item.id}>
+              <Image
+                src={item.urls.regular}
+                w="100%"
+                h="100%"
+                borderRadius="3px"
+              />
+            </GridItem>
+          ))}
+        </Grid>
       </Box>
     </Container>
   );
+}
+
+export async function getServerSideProps(context) {
+  const page = context.query.page ?? '';
+  const clientId = 'El0dCOXoX5HrqE4QNSTKPXPkkWcq3tBaAPoaZWgWHPc';
+  const url = `https://api.unsplash.com/photos?page=${page}&per_page=9&client_id=${clientId}`;
+  const res = await fetch(url);
+  const dataPhotos = await res.json();
+  return {
+    props: {
+      dataPhotos,
+    },
+  };
 }
